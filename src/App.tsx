19d8477c738bf8d4a02424ee29117982e8641dc2
towmie <andre00toz@gmail.com>
2024-06-10 16:06:36 +0300

    feat: complete routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import { Route, Routes } from "react-router-dom";
 import "./globals.css";
 import SigninForm from "./_auth/forms/SigninForm";
-import { Home } from "./_root/pages/";
+import {
+  AllUsers,
+  CreatePost,
+  Explore,
+  Home,
+  Profile,
+  Saved,
+  UpdatePost,
+  UpdateProfile,
+} from "./_root/pages/";
 import SignUpForm from "./_auth/forms/SignUpForm";
 import AuthLayout from "./_auth/AuthLayout";
 import RootLayout from "./_root/RootLayout";
 import { Toaster } from "@/components/ui/toaster";
+import PostDetails from "./_root/pages/PostDetails";
 
 function App() {
   return (
