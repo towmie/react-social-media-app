2790621a3021c7599bcc258c947c0b9d219eb191
towmie <andre00toz@gmail.com>
2024-06-12 13:36:09 +0300

    feat: add search results

--- src/components/shared/SearchResults.tsx
+++ src/components/shared/SearchResults.tsx
@@ -1,5 +1,24 @@
-function SearchResults() {
-  return <div></div>;
+import { Models } from "appwrite";
+import Loader from "./Loader";
+import GridPostList from "./GridPostsList";
+
+type SearchResultsProps = {
+  isSearchFetching: boolean;
+  searchedPosts: Models.Document[];
+};
+
+function SearchResults({
+  isSearchFetching,
+  searchedPosts,
+}: SearchResultsProps) {
+  if (isSearchFetching) return <Loader />;
+
+  if (searchedPosts && searchedPosts.documents.length > 0)
+    return <GridPostList posts={searchedPosts.documents} />;
+
+  return (
+    <p className="text-light-4 mt-10 text-center w-full">No Results Found</p>
+  );
 }
 
 export default SearchResults;
